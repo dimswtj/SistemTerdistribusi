5710f53a274ca0cf9fa4261654c357d2beb44ae3
dimswtj <114901368+dimswtj@users.noreply.github.com>
2023-07-06 15:54:17 +0800

    Add files via upload

---
+++ latweb/web/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="w-screen h-screen flex justify-center  items-center">
+    <div class="max-w-lg mx-auto my-10 bg-white border-solid border-2 border-indigo-600 rounded-lg shadow-md p-5">
+        <img class="w-32 h-32 rounded-full mx-auto object-cover" src="https://static1.undiksha.ac.id/kkn/2023/upload/profile/20634906201685344972.foto%20jas%20bg%20merah.png" alt="Profile picture">
+        <h2 class="text-center text-2xl font-semibold mt-3">Putu Gede Dimas Witjaksana</h2>
+        <p class="text-center text-gray-600 mt-1">2115101045</p>
+        <div class="flex justify-center mt-5">
+            <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">Twitter</a>
+            <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">LinkedIn</a>
+            <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">GitHub</a>
+        </div>
+        <div class="mt-5">
+        <h3 class="text-xl font-semibold">Bio</h3>
+        <p class="text-gray-600 mt-2">Introducing my name is Putu Gede Dimas Witjaksana from the computer science study program, department of informatics engineering, faculty of engineering and vocational education university ganesha</p>
+        </div>
+    </div>
+</body>
+</html>
